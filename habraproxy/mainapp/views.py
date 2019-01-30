2b8894d540f4f2ffd162bfe47d8dacc3be7fdf00
andrepopoff <andrepopoff@gmail.com>
2019-01-30 20:41:42 +0400

    Fixed incorrect display of plus signs (views.py)

--- habraproxy/mainapp/views.py
+++ habraproxy/mainapp/views.py
@@ -15,9 +15,12 @@ def set_jquery_script_tag(html, src):
 
 
 def proxy_view(request, url):
-    raw_html = get('https://habr.com/' + url).text
-    changed_urls_html = raw_html.replace('href="https://habr.com/', 'href="http://{}/'.format(request.get_host()))
-    html = BeautifulSoup(changed_urls_html, 'lxml')
+    source_html = get('https://habr.com/' + url).text
+
+    html_with_right_urls = source_html.replace('href="https://habr.com/', 'href="http://{}/'.format(request.get_host()))
+    html_with_right_pluses = html_with_right_urls.replace('&plus;', '&#43;')
+
+    html = BeautifulSoup(html_with_right_pluses, 'lxml')
 
     set_jquery_script_tag(html, 'https://code.jquery.com/jquery-3.1.1.js')
     set_jquery_script_tag(html, '/static/js/addTMs.js')
