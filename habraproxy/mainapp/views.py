563c254be12351016898c539d6a53045d05e44c2
andrepopoff <andrepopoff@gmail.com>
2019-01-30 21:24:08 +0400

    Added docstrings to proxy_view func (views.py)

--- habraproxy/mainapp/views.py
+++ habraproxy/mainapp/views.py
@@ -15,14 +15,20 @@ def set_jquery_script_tag(html, src):
 
 
 def proxy_view(request, url):
+    """
+    The view that is responsible for displaying the site https://habr.com/.
+    Appends ™ to all 6 letter words
+    """
     source_html = get('https://habr.com/' + url).text
-
     html_with_right_urls = source_html.replace('href="https://habr.com/', 'href="http://{}/'.format(request.get_host()))
-    html_with_right_pluses = html_with_right_urls.replace('&plus;', '&#43;')
 
+    # I had to change the code for the plus signs, because they were displayed incorrectly
+    html_with_right_pluses = html_with_right_urls.replace('&plus;', '&#43;')
     html = BeautifulSoup(html_with_right_pluses, 'lxml')
 
+    # Just in case, added the jQuery CDN script to HTML
     set_jquery_script_tag(html, 'https://code.jquery.com/jquery-3.1.1.js')
-    set_jquery_script_tag(html, '/static/js/addTMs.js')
 
+    # Added jQuery script to HTML that adds ™ to 6 letter words
+    set_jquery_script_tag(html, '/static/js/addTMs.js')
     return HttpResponse(str(html))
